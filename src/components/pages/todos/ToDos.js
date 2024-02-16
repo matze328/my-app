748b64a231f536c6d41ed1e8d7f3e868a57bf56a
matze328 <152269645+matze328@users.noreply.github.com>
2024-02-16 14:42:36 +0100

    todo item hinzugefügt und checkbox init und loading div nach gebessert

--- src/components/pages/todos/ToDos.js
+++ src/components/pages/todos/ToDos.js
@@ -1,20 +1,40 @@
-import { useEffect, useState } from "react";
-import styles from "./ToDos.module.css" 
+import ToDoItem from "../../common/templates/todo-item";
+import styles from "./ToDos.module.css";
+import { useState, useEffect } from "react";
 
+function ToDoPage() {
+  //Da wir die Werte erst aus der API mit einem HTTP GET holen müssen,
+  //nutzen wir die States von React aus, die bei Veränderung ein
+  //neurendern der Komponente triggern
+  //Antwort ist da? => lad die Komponente einmal mit Daten gefüllt neu
+  const [todos, setTodos] = useState([]);
 
-function ToDos() {
-    const [todos, setTodos] = useState([]);
+  //useEffekt hier: damit wir den folgenden Code nur einmal ausführen,
+  //obwohl wir mit setTodos den State von todos ändern
+  useEffect(() => {
+    fetch('http://localhost:3030/v1/todos/alltodos')
+      .then((response) => response.json())
+      .then((todosJson) => {
+        console.log(todosJson);
+        setTodos(todosJson);
+      }); //setTodos: todos = todosJson
+  }, []);
 
-    fetch("http://localhost:3030/v1/todos/alltodos")
-        .then(response => response.json())
-        .then(json => setTodos(json));
+  //###Ergänzung zum Code vom Unterricht:###
+  //Wenn todos noch leer ist, gib leeren Container aus
+  if (todos.length == 0) {
+    return <div className={styles.mainContainer}></div>;
+  }
 
-    return ( <div className={styles.mainContainer}>
-        "Das ist meine ToDos Site" 
-        <ToDoItem></ToDoItem>
-        <ToDoItem></ToDoItem>
-        </div>
-    )
+  //wenn wir die Daten in todos drin haben (durch den fetch)
+  //können wir auch todos, sowie dessen Inhalte in Probs weitergeben
+  //hier: "todo={todos[1]}" gibt das 2. todo weiter.
+  return (
+    <div className={styles.mainContainer}>
+      <ToDoItem todo={todos[0]}></ToDoItem>
+      <ToDoItem todo={todos[1]}></ToDoItem>
+    </div>
+  );
 }
 
-export default ToDos;
+export default ToDoPage;
